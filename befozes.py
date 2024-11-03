1a5886d3ebf9fd90ffe01236e2561915bcc3e425
Blaj3n <benceulrich@gmail.com>
2024-11-03 07:48:21 +0100

    4. feladat elegendő üveg mennyiség

--- befozes.py
+++ befozes.py
@@ -3,8 +3,8 @@ uvegek = [5, 2, 2, 4, 3, 2, 4, 10, 5, 5, 3, 5, 4, 3, 3]
 print(uvegek)
 
 print("2. feladat")
-lekvar_dl = sum(uvegek)
-print(f"Mari néni lekvárja (dl): {lekvar_dl}")
+osszeslekvar = sum(uvegek)
+print(f"Mari néni lekvárja (dl): {osszeslekvar}")
 
 print("3. feladat")
 max_szam = 0
@@ -16,3 +16,11 @@ for i, uveg in enumerate(uvegek):
         pozicio = i + 1
 
 print(f"A legnagyobb üveg {max_szam} deciliteres és a {pozicio}. a sorban.")
+
+print("4. feladat")
+
+elegendo_uveg = sum(uvegek)
+if elegendo_uveg >= osszeslekvar:
+    print("Elegendő üveg volt.")
+else:
+    print("Maradt lekvár.")

a38350effe36c778f7001892fa4a1f4ccbadbab0
Blaj3n <benceulrich@gmail.com>
2024-11-03 07:06:53 +0100

    2. feladat üvegek összege

--- befozes.py
+++ befozes.py
@@ -1,2 +1,9 @@
+print("1. feladat")
 uvegek = [5, 2, 2, 4, 3, 2, 4, 10, 5, 5, 3, 5, 4, 3, 3]
 print(uvegek)
+
+print("2. feladat")
+lekvar_dl = sum(uvegek)
+print(f"Mari néni lekvárja (dl): {lekvar_dl}")
+
+

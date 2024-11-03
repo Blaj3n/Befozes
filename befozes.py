e24aa24ad51798bb57fa2bee6063d414329b3e20
Blaj3n <benceulrich@gmail.com>
2024-11-03 07:28:56 +0100

    3. feladat legnagyobb üveg

--- befozes.py
+++ befozes.py
@@ -6,4 +6,13 @@ print("2. feladat")
 lekvar_dl = sum(uvegek)
 print(f"Mari néni lekvárja (dl): {lekvar_dl}")
 
+print("3. feladat")
+max_szam = 0
+pozicio = 0
 
+for i, uveg in enumerate(uvegek):
+    if uveg > max_szam:
+        max_szam = uveg
+        pozicio = i + 1
+
+print(f"A legnagyobb üveg {max_szam} deciliteres és a {pozicio}. a sorban.")
